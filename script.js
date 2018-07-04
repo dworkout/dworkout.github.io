095cfb863fe7f4e4653efddc1f2f9f570ed10880
findeer <38536932+findeer@users.noreply.github.com>
2018-07-04 13:22:13 +0300

    AddProg

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $('document').ready(function() {
     $('.btnDark').on('click', function() {
         $('.a1').toggleClass('dark');
-        $('nav img, .btnDark, .question, .s span, .s, .time p, .time img, .text-1, .activeBtn').toggleClass('inverse');
-        $('.time').toggleClass('time-dark')
+        $('nav img, .btnDark, .question, .s span, .s, .time p, .time img, .text-1, .activeBtn, .titleP').toggleClass('inverse');
+        $('.time').toggleClass('time-dark');
         $('nav').toggleClass('darkNav');
-        $('.bigbtn a').toggleClass('bigbtnDark')
+        $('.bigbtn a').toggleClass('bigbtnDark');
         $('body').toggleClass('darkBg');
         var src = ($('.bg').attr('src') === 'backgroundFixed.png')
             ? 'backgroundDark.png'
