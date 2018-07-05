66faba967892023024d1edafb2adc789b43c44e1
findeer <38536932+findeer@users.noreply.github.com>
2018-07-06 00:32:21 +0300

    fixBug

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $('document').ready(function() {
         $('nav img, .btnDark, .question, .s span, .s, .time p, .time img, .text-1, .activeBtn, .titleP, .darkVleft, .mob-a').toggleClass('inverse');
         $('.time').toggleClass('time-dark');
         $('nav').toggleClass('darkNav');
+        $('.bg-other').toggleClass('bgBlack');
         $('.bigbtn a').toggleClass('bigbtnDark');
         $('body').toggleClass('darkBg');
         var src = ($('.bg').attr('src') === 'backgroundFixed.png')
