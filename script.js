7e4b810a978891d7a804c144f16bafd00641dc33
findeer <38536932+findeer@users.noreply.github.com>
2018-07-06 00:14:00 +0300

    addMobV

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('document').ready(function() {
     $('.btnDark').on('click', function() {
         $('.a1').toggleClass('dark');
-        $('nav img, .btnDark, .question, .s span, .s, .time p, .time img, .text-1, .activeBtn, .titleP').toggleClass('inverse');
+        $('nav img, .btnDark, .question, .s span, .s, .time p, .time img, .text-1, .activeBtn, .titleP, .darkVleft, .mob-a').toggleClass('inverse');
         $('.time').toggleClass('time-dark');
         $('nav').toggleClass('darkNav');
         $('.bigbtn a').toggleClass('bigbtnDark');
@@ -10,16 +10,10 @@ $('document').ready(function() {
             ? 'backgroundDark.png'
             : 'backgroundFixed.png';
         $('.bg').attr('src', src);
-    })
+    });
     
-    $(window).resize(function chMobileScale() {
-        var viewportWidth = $(window).width();
-        if (viewportWidth > 768) {
-                $(".img").removeClass("col-12").addClass("col-6");
-                $(".btnDark").attr('src', 'https://png.icons8.com/ios/65/000000/black-tie-filled.png');
-        } else {
-                $(".img").removeClass("col-6").addClass("col-12");
-                $(".btnDark").attr('src', 'https://png.icons8.com/ios/43/000000/black-tie-filled.png');
-        }
+    $('.menu-toggle').on('click',function(){
+        $('.menu-toggle').toggleClass('active');
+        $('.mob-a').toggleClass('d-none');
     });
 })
